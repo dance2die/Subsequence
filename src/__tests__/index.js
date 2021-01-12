8f722e59362bd07d44c6a63ab7f7b531a17b892f
Sung M. Kim <sungmkim80@gmail.com>
2021-01-11 23:52:49 -0500

    added another test for when input === sequence

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -6,3 +6,10 @@ test('Happy Path', () => {
 
   expect(validate(sequence, input)).toBe(true)
 })
+
+test('that if input and sequence are the same, sequence is the subsequence of the input', () => {
+  const input = [5, 1, 22, 25, 6, -1, 8, 10]
+  const sequence = [...input]
+
+  expect(validate(sequence, input)).toBe(true)
+})
